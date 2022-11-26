ec5782bd96f4671ba12483bb78d1d8b14eff7e6f
Futureland3 <markmurai3@gmail.com>
2022-11-26 20:41:25 +0100

    Add HTML, CSS, font and gitub logo

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// add, subtract, multiply, divide 
+
+//What the fuck does this even mean:
+//Create a new function operate that takes an operator and 2 numbers 
+//and then calls one of the above functions on the numbers.
+
+a = 10;
+b = 5;
+console.log(a * b);
+
+if (a + b === 15) {
+    console.log("Pussyboi");
+}
