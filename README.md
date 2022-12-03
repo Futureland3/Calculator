013712ae6e18e551444194c1fc4d88ccda1dd838
Futureland3 <markmurai3@gmail.com>
2022-12-03 13:37:30 +0100

    Change button html, Steal others' codes, Cry cuz it still doesn't work, Make every function work except the equal and the dot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+
+If you press on the Github logo it makes a rotating animation haha, also it takes me to my Github.
--- javascript.js
+++ javascript.js
@@ -1,13 +1,129 @@
-// add, subtract, multiply, divide 
+// let button = document.querySelectorAll('[data-number]').forEach(link => 
+//     link.addEventListener('click',() => {
+//         // return document.getElementById('result').innerHTML = button;
+//         console.log(button);
+// }));
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
 
-//What the fuck does this even mean:
-//Create a new function operate that takes an operator and 2 numbers 
-//and then calls one of the above functions on the numbers.
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalButton = document.getElementById('equal');
+const clearButton = document.getElementById('clear');
+const deleteButton = document.getElementById('delete');
+const dotButton = document.getElementById('dot');
+const displayResult = document.getElementById('result');
 
-a = 10;
-b = 5;
-console.log(a * b);
+equalButton.addEventListener('click', evaluate )
+clearButton.addEventListener('click', clear)
+dotButton.addEventListener('click', dot)
+deleteButton.addEventListener('click', dlt)
 
-if (a + b === 15) {
-    console.log("Pussyboi");
+function add(a, b) {
+    return (a + b);
 }
+
+function subtract(a, b){
+    return (a - b);
+}
+
+function multiply(a, b){
+    return (a * b);
+}
+
+function divide(a, b){
+    return (a / b);
+}
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return '÷'
+    if (keyboardOperator === '*') return 'x'
+    if (keyboardOperator === '-') return '-'
+    if (keyboardOperator === '+') return '+'
+  }
+
+numberButtons.forEach ((button) =>
+    button.addEventListener('click',() => appendNumber(button.textContent))
+);
+
+function appendNumber(number) {
+    if (displayResult.textContent === '0' || shouldResetScreen === true)
+      reset();
+    displayResult.textContent += number
+  }
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click',() => appendNumber(button.textContent))
+  );
+
+function reset() {
+    displayResult.textContent = ''
+    shouldResetScreen = false //this line is actually not that needed
+  }
+
+  function operate(a, operator, b) {
+    a = Number(a);
+    b= Number(b);
+    switch (operator) {
+        case '+':
+            return add();
+        case '-':
+            return subtract();
+        case 'x':
+            return multiply();
+        case '÷':
+            if (b === 0) return null
+      else return divide(a, b)
+    default:
+      return null
+    }
+}
+
+function dot() {
+    if (shouldResetScreen) reset()
+    if (displayResult.textContent === '')
+      displayResult.textContent = '0'
+    if (displayResult.textContent.includes('.')) return
+    displayResult.textContent += ''
+  }
+
+function clear() {
+    displayResult.textContent = '0'
+    firstOperand = ''
+    secondOperand = ''
+    currentOperation = null
+  }
+
+function dlt() {
+  displayResult.textContent = displayResult.textContent
+    .toString()
+    .slice(0, -1)
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate()
+  firstOperand = displayResult.textContent
+  currentOperation = operator
+  displayResult.textContent = `${firstOperand} ${currentOperation}`
+  shouldResetScreen = true
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && displayResult.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  secondOperand = displayResult.textContent
+  displayResult.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  )
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+  currentOperation = null
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+  }
